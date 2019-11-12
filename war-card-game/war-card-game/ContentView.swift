d662f3ff554e85c50ce2bde989d85e5db57a4fe4
luicurtis <lui_curtis@hotmail.com>
2019-11-12 10:03:32 -0800

    Added card assets

---
+++ war-card-game/war-card-game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  war-card-game
+//
+//  Created by Curtis Lui on 2019-11-12.
+//  Copyright © 2019 Curtis Lui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
